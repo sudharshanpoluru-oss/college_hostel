8cb7678098858f52f406a632b75baad84aa82f5d
sudharshanpoluru-oss <sudharshanpoluru@gmail.com>
2026-07-03 14:38:38 +0530

    Fix Express 5 wildcard route syntax

--- server/index.cjs
+++ server/index.cjs
@@ -20,7 +20,7 @@ app.use('/api/warden', wardenRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'dist')));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
   });
 }
